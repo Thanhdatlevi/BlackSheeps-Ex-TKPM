275e681e60f061e2c00547b574c7f12d899ef543
botbienn <vubac2004@gmail.com>
2025-03-11 15:52:25 +0700

    almost finished updating

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -5,4 +5,9 @@ const studentController = require('../modules/student/studentController');
 
 router.get('/', studentController.addStudent);
 
+// update student
+router.get('/update', studentController.updateStudentPage);
+router.get('/update/:mssv', studentController.updateQueryStudent);
+router.put('/update', studentController.updateStudent);
+
 module.exports = router;
